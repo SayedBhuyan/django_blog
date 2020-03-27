4a6ccd78708defc6ffd72d767b30886391c6cc74
Sayed Bhuyan <syeedredom@gmail.com>
2020-03-27 19:21:55 -0400

    added login required to create and updated post functionality

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
 # from django.views.generic import ListView
-from django.views.generic import ListView
-from django.views.generic import DetailView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView
+)
 
 def home(request):
     context = {
@@ -19,6 +24,21 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
 
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
     return render(request, "blog/about.html", {'title': 'About'})
