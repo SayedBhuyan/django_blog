72af44fc9a4f50b41aaee0285023df921b4a2a16
Sayed Bhuyan <syeedredom@gmail.com>
2020-03-27 10:02:53 -0400

    added detail view

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from .models import Post
+# from django.views.generic import ListView
+from django.views.generic import ListView
+from django.views.generic import DetailView
 
 def home(request):
     context = {
         "posts": Post.objects.all()
     }
     return render(request, "blog/home.html", context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = "posts"
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
 
 def about(request):
     return render(request, "blog/about.html", {'title': 'About'})
